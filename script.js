70fdd9b0e397d6119228218f2902d4d94e4c2566
Yaroslav Filipenko <yarikfilipenko@gmail.com>
2023-03-14 21:34:06 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+$('.rating__circle').click(function() {
+    $(this).addClass('active');
+    $('.rating__circle').not(this).removeClass('active');
+});
+
+$('.submit').click(function() {
+    $('.main-wrapper').css('display', 'none');
+    $('.state-wrapper').css('display', 'flex');
+
+   if($('.rating__circle').hasClass('active')) {
+        let ratingValue = $('.rating__circle.active').prop("checked", true).val();
+            $('.state-selected-info').text('You selected ' + ratingValue + ' out of 5');
+   } else {
+        $('.state-selected-info').text('You selected 0 out of 5');
+   }
+});
